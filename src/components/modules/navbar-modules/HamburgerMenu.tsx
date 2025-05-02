82c037c4d0fa27803a7deb8cd20a83fdf3a7a375
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-02 05:25:18 -0400

    FOOTER

--- src/components/modules/navbar-modules/HamburgerMenu.tsx
+++ src/components/modules/navbar-modules/HamburgerMenu.tsx
@@ -16,7 +16,7 @@ const HamburgerMenu = () => {
             <Button onClick={toggleDrawer(true)} style={{ backgroundColor: 'transparent', border: 'none', minWidth: 0, minHeight: 0 }}>
                 <FaBars className="w-8 h-8 text-[var(--text-primary)]" />
             </Button>
-            <Drawer open={open} onClose={toggleDrawer(false)} transitionDuration={300} translate='yes' className='bg-black/50 backdrop-blur-xl z-[1000]'>
+            <Drawer open={open} onClose={toggleDrawer(false)} transitionDuration={300} translate='yes' className='bg-black/50 backdrop-blur-xs z-[1000]'>
                 <div className="p-4 bg-[var(--main)] text-[var(--text-primary)] h-full overflow-scroll w-[230px]">
                     <HamburgerMenuContent />
                 </div>
