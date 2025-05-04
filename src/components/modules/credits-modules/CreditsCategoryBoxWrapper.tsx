8243a03989af9db8050fbc1493e3ad54676dc502
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-04 04:23:06 -0400

    HAMBURGER MENU

--- src/components/modules/credits-modules/CreditsCategoryBoxWrapper.tsx
+++ src/components/modules/credits-modules/CreditsCategoryBoxWrapper.tsx
@@ -1,21 +1,27 @@
+import { BsRocketTakeoffFill } from "react-icons/bs"; 
 import React from 'react'
 import CreditsCategorySingleBox from './CreditsCategorySingleBox';
 
 const CreditsCategoryBoxWrapper = () => {
   const [selectedCard, setSelectedCard] = React.useState(0);
   const cards = [
-    {id :1 , title: 'Card 1', description: 'Description 1' },
-    {id: 2, title: 'Card 2', description: 'Description 2' },
-    {id: 3, title: 'Card 3', description: 'Description 3' },
+    {id :1 , title: 'Card 1', description: 'Description 1' , icon: BsRocketTakeoffFill },
+    { id: 2, title: 'Card 2', description: 'Description 2', icon: BsRocketTakeoffFill },
+    { id: 3, title: 'Card 3', description: 'Description 3', icon: BsRocketTakeoffFill },
+    { id: 4, title: 'Card 4', description: 'Description 4', icon: BsRocketTakeoffFill },
+    { id: 5, title: 'Card 5', description: 'Description 5', icon: BsRocketTakeoffFill },
+    { id: 6, title: 'Card 6', description: 'Description 6', icon: BsRocketTakeoffFill },
+    { id: 7, title: 'Card 7', description: 'Description 7', icon: BsRocketTakeoffFill },
   ];
   
   return (
-    <>
+    <div className="flex flex-wrap justify-center">
       {cards.map((card, index) => (
         <CreditsCategorySingleBox key={card.id} card={card} index={index} selectedCard={selectedCard} setSelectedCard={setSelectedCard} />
       ))}
-    </>
+    </div>
   )
 }
 
 export default CreditsCategoryBoxWrapper
+
--- src/components/modules/credits-modules/CreditsCategorySingleBox.tsx
+++ src/components/modules/credits-modules/CreditsCategorySingleBox.tsx
@@ -1,9 +1,11 @@
 import { Card, CardActionArea, CardContent, Typography } from '@mui/material'
+import React from 'react';
 
 interface CardProps {
   id: number;
   title: string;
   description: string;
+  icon: React.ElementType;
 }
 interface CreditsCategorySingleBoxProps {
   card: CardProps;
@@ -14,29 +16,29 @@ interface CreditsCategorySingleBoxProps {
 // COMPONENT ===============================================================================================================================================================================
 const CreditsCategorySingleBox:React.FC<CreditsCategorySingleBoxProps> = ({ card, index, selectedCard, setSelectedCard }) => {
   return (
-    <Card key={card.id} sx={{ width: 300, height: '130px', margin: 2, borderRadius: 2, boxShadow: 3  ,display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-    <CardActionArea
-      onClick={() => setSelectedCard(index)}
-      data-active={selectedCard === index ? '' : undefined}
-      sx={{
-        backgroundColor: selectedCard === index ? 'primary.main' : 'background.paper',
-        height: '100%',
-        borderRadius: 2,
-        color: selectedCard === index ? 'white' : 'text.primary',
-        transition: 'background-color 0.3s ease',
-        '&:hover': { backgroundColor: selectedCard === index ? 'primary.dark' : 'action.hover', },
-      }}
-  >
-    <CardContent sx={{ height: '100%', display: 'flex' , flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-      <Typography variant="h4" component="div" sx={{ fontSize: 40 }}>
-        {card.id}
-      </Typography>
-      <Typography variant="h5" component="div">
-        {card.title}
-      </Typography>
-        <Typography variant="body2" color="text.secondary">  {card.description}</Typography>
-    </CardContent>
-  </CardActionArea>
+      <Card key={card.id} sx={{ width: "100%", height: '130px', margin: 2, borderRadius: 2, boxShadow: 3, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <CardActionArea
+              onClick={() => setSelectedCard(index)}
+              data-active={selectedCard === index ? '' : undefined}
+              sx={{
+                  backgroundColor: selectedCard === index ? 'primary.main' : 'background.paper',
+                  height: '100%',
+                  borderRadius: 2,
+                  color: selectedCard === index ? 'white' : 'text.primary',
+                  transition: 'background-color 0.3s ease',
+                  '&:hover': { backgroundColor: selectedCard === index ? 'primary.dark' : 'action.hover', },
+              }}
+          >
+              <CardContent sx={{ height: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                  <div>
+                      {React.createElement(card.icon, { style: { fontSize: '40px' } })}
+                  </div>
+                  <Typography variant="h5" component="div">
+                      {card.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">  {card.description}</Typography>
+              </CardContent>
+          </CardActionArea>
       </Card>
   )
 }
