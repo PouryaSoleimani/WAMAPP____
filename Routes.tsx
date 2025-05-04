a64442bf9acefe0058bf985ce8c3da63e90ab8ab
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-04 03:04:24 -0400

    HAMBURGER MENU

--- Routes.tsx
+++ Routes.tsx
@@ -5,16 +5,4 @@ import  HomePage  from "./src/pages/HomePage";
 
 export const router = createBrowserRouter([
     { path: '/', element: <HomePage /> },
-    // //^PRODUCTS
-    // {
-    //  path: '/products', element: <Products />, children: [
-    //   { path: 'all', element: <AllProducts /> },
-    //   { path: 'men', element: <MenProducts /> },
-    //   { path: 'women', element: <WomenProducts /> },
-    //   { path: 'kids', element: <KidsProducts /> },
-    //   { path: 'sale', element: <SaleProducts /> },
-    //   { path: 'jerseys', element: <KitsProducts /> },
-    //   { path: 'productpage', element: <ProductPage /> },ّ
-    //  ]
-    // }
-   ])
+])
