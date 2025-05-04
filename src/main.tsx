da5aa564e3c26c8e81dbe2a2c251c1467400bf3e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-04 03:05:27 -0400

    HAMBURGER MENU

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './pages/HomePage.tsx'
+import NavBarComponent from './components/templates/NavBarComponent.tsx'
+import FooterComponent from './components/templates/FooterComponent.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <NavBarComponent />
     <App />
+    <FooterComponent />
   </StrictMode>,
 )
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,25 +1,17 @@
 // ^ HOME PAGE ==================================================================================================================================================
 import HeaderButtonsTopComponent from "../components/modules/header-modules/HeaderButtonsTopComponent";
 import HeaderTitleComponent from "../components/modules/header-modules/HeaderTitleComponent";
-import NavBarComponent from "../components/templates/NavBarComponent";
 import CategoryFlexSection from "../components/templates/CategoryFlexSection";
 import CategoryGridSection from "../components/templates/CategoryGridSection";
-import FooterComponent from "../components/templates/FooterComponent";
 
 function HomePage() {
       return (
-            <>
-                  <NavBarComponent />
-
                   <main className="mx-auto relative inset-0 text-center z-10 h-auto mt-[55px] p-2" dir="rtl">
                         <HeaderTitleComponent />
                         <HeaderButtonsTopComponent />
                         <CategoryFlexSection />
                         <CategoryGridSection />
                   </main>
-
-                  <FooterComponent />
-            </>
       );
 }
 
