bfa0e46e4844179c50db158031b2e368dc763c8c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-05 03:58:22 -0400

    SPECIAL LOAN PAGE

--- src/components/modules/special-loans-modules/FilterDropDown.tsx
+++ src/components/modules/special-loans-modules/FilterDropDown.tsx
@@ -1,12 +1,23 @@
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
-import db from './db/db.json'; 
+const cities = [
+  { "id": 1, "name": "تهران" },
+  { "id": 2, "name": "شیراز" },
+  { "id": 3, "name": "اصفهان" },
+  { "id": 4, "name": "مشهد" },
+  { "id": 5, "name": "تبریز" },
+  { "id": 6, "name": "کرمان" },
+  { "id": 7, "name": "یزد" },
+  { "id": 8, "name": "قم" },
+  { "id": 9, "name": "کرج" },
+  { "id": 10, "name": "اهواز" }
+]
 
-export type citiesArrayType = [{id : number, name : string}];
+export type citiesArrayType = {id : number, name : string}[];
 
 const FilterDropDown = () => {
 
-  const cityOptions: citiesArrayType = db.cities.map((city: { id: number; name: string }) => ({ id: city.id, name: city.name }));
+  const cityOptions = cities.map((city: { id: number; name: string }) => ({ id: city.id, name: city.name }));
   
   return (
     <Autocomplete
