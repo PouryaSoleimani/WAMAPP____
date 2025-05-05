aa1d31c558c9d8d9d15126dd0c26699f8f85fc09
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-05 03:50:59 -0400

    SPECIAL LOAN PAGE

--- Routes.tsx
+++ Routes.tsx
@@ -2,10 +2,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import  HomePage  from "./src/Routes/HomePage";
 import CreditPage from "./src/Routes/CreditPage";
+import SpecialLoansPage from "./src/Routes/SpecialLoansPage";
 
 
 
 export const router = createBrowserRouter([
     { path: '/', element: <HomePage /> },
     { path: '/credits', element: <CreditPage /> },
+    { path: '/special-loans', element: <SpecialLoansPage /> },
 ])
---
+++ src/types/json.d.ts
@@ -0,0 +1,4 @@
+declare module "*.json" {
+    const value: citiesArrayType;
+    export default value;
+  }
