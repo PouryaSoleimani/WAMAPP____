7fbb34eca7be7ac96f65899024336ef2dbc64c0f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-05 02:22:35 -0400

    HOME PAGE

--- src/components/modules/credits-modules/CreditsCategoryBoxWrapper.tsx
+++ src/components/modules/credits-modules/CreditsCategoryBoxWrapper.tsx
@@ -1,23 +1,18 @@
-import { BsRocketTakeoffFill } from "react-icons/bs"; 
 import React from 'react'
 import CreditsCategorySingleBox from './CreditsCategorySingleBox';
 
 const CreditsCategoryBoxWrapper = () => {
   const [selectedCard, setSelectedCard] = React.useState(0);
   const cards = [
-    {id :1 , title: 'تسهیلات آموزش  و تحصیل', icon: BsRocketTakeoffFill },
-    { id: 2, title: 'تسهیلات دوم', icon: BsRocketTakeoffFill },
-    { id: 3, title: 'تسهیلات سوم', icon: BsRocketTakeoffFill },
-    { id: 4, title: 'تسهیلات چهارم', icon: BsRocketTakeoffFill },
-    { id: 5, title: 'تسهیلات پنجم', icon: BsRocketTakeoffFill },
-    { id: 6, title: 'تسهیلات ششم', icon: BsRocketTakeoffFill },
-    { id: 7, title: 'تسهیلات هفتم', icon: BsRocketTakeoffFill },
+    {id :1 , title: 'وام تخصصی', desc : "هر کالایی که بخوای بخری با هر نیازی که داری ما بهت وام میدیم" , isShowVamapp : true  ,isShowCountryComps: false},
+    { id: 2, title: 'معرفی وام های کشوری', desc: "شرکت های معتبر وام دهی در کشور",  isShowVamapp : false , isShowCountryComps : true },
+    { id: 3, title: '  وام های استانی', desc: "وام دهندگان محلی در استان شما", isShowVamapp : false , isShowCountryComps: false },
   ];
   
   return (
     <div className="flex flex-wrap justify-center">
       {cards.map((card, index) => (
-        <CreditsCategorySingleBox key={card.id} card={card} index={index} selectedCard={selectedCard} setSelectedCard={setSelectedCard} />
+        <CreditsCategorySingleBox key={card.id} card={card} index={index} selectedCard={selectedCard} setSelectedCard={setSelectedCard} isShowVamapp={card.isShowVamapp} isShowCountryComps={card.isShowCountryComps} />
       ))}
     </div>
   )
--- src/components/modules/credits-modules/CreditsCategorySingleBox.tsx
+++ src/components/modules/credits-modules/CreditsCategorySingleBox.tsx
@@ -1,34 +1,57 @@
 import { FaChevronLeft } from "react-icons/fa";
-import { Card, CardActionArea, CardContent } from "@mui/material";
+import { Button, Card, CardActionArea, CardContent } from "@mui/material";
 import React from "react";
+import { Link } from "react-router";
 
 interface CardProps {
       id: number;
       title: string;
-      icon: React.ElementType;
+      desc: string;
+      isShowVamapp : boolean;
+      isShowCountryComps : boolean;
 }
 interface CreditsCategorySingleBoxProps {
       card: CardProps;
       index: number;
       selectedCard: number;
       setSelectedCard: (index: number) => void;
+      isShowVamapp: boolean;
+      isShowCountryComps: boolean
 }
-// COMPONENT ===============================================================================================================================================================================
-const CreditsCategorySingleBox: React.FC<CreditsCategorySingleBoxProps> = ({ card, index, selectedCard, setSelectedCard }) => {
+const CreditsCategorySingleBox: React.FC<CreditsCategorySingleBoxProps> = ({ card, index, selectedCard, setSelectedCard, isShowVamapp, isShowCountryComps }) => {
       return (
-            <Card key={card.id} sx={{ width: "100%", height: "auto", margin: 2, borderRadius: 2, boxShadow: 3, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-                  <CardActionArea onClick={() => setSelectedCard(index)} data-active={selectedCard === index ? "" : undefined} sx={{ backgroundColor: selectedCard === index ? "info.main" : "background.paper", height: "100%", borderRadius: 2, color: selectedCard === index ? "dark" : "text.primary", transition: "background-color 0.3s ease", "&:hover": { backgroundColor: selectedCard === index ? "info.dark" : "action.hover" } }}>
-                        <CardContent sx={{ height: "100%", display: "flex", flexDirection: "column", gap: "8px", justifyContent: "space-between", alignItems: "center" }}>
+            <Card key={card.id} sx={{ width: "100%", height: "auto", margin: "10px 1.5px", borderRadius: 2, boxShadow: 2, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center",border : "none"}}>
+                  <CardActionArea
+                        onClick={() => setSelectedCard(index)} 
+                        data-active={selectedCard === index ? "" : undefined}
+                        sx={{ backgroundColor: selectedCard === index ? "var(--home-bg)" : "#cecece", height: "100%", borderRadius: 2, color: selectedCard === index ? "dark" : "text.primary", transition: "background-color 0.3s ease", "&:hover": { backgroundColor: selectedCard === index ? "var(--home-bg-hover)" : "var(--home-bg)" } }}>
+                        <CardContent sx={{ height: "100%", display: "flex", flexDirection: "column", gap: "16px",padding : "10px", justifyContent: "space-between", alignItems: "center" }}>
                               <div id="TOP" className="flex items-center justify-between w-full">
-                                    {React.createElement(card.icon, { style: { fontSize: "40px", backgroundColor: "#cccccc9d", padding: "3px", borderRadius: "8px" } })}
-                                    <h3 className="font-bold text-md tracking-tight">{card.title}</h3>
+                                    {/* {React.createElement(card.icon, { style: { fontSize: "40px", backgroundColor: "#cccccc9d", padding: "3px", borderRadius: "8px" } })} */}
+                                    <h3 className="font-bold text-lg ">{card.title}</h3>
                                     <FaChevronLeft />
                               </div>
 
-                              <div className="flex flex-col gap-y-4 items-stretch justify-between w-full">
-                                    <div className="flex w-full items-center justify-between bg-[#cccccc9d] px-3 py-2 rounded-md mt-4">
-                                          <h3>با اعتبار سنجی</h3>
-                                          <h3>1 تا 10 میلیون تومان</h3>
+                              <div className="flex flex-col items-stretch justify-between w-full">
+                                    <div className="flex w-full items-center justify-between gap-4 text-justify">
+                                          <p className="text-xs w-[60%]">{card.desc}</p>
+                                          {isShowVamapp === true && (
+                                                <img src="/logo.png" alt="logo" className="w-12 h-4 ml-1" />
+                                          )}
+                                    </div>
+                                    <div className="mt-4 flex items-center justify-around">
+                                          {isShowCountryComps === true && (
+                                                <>
+                                                <img src="/company_logos/digipay.png" alt="Country Comps Logo" className="size-8" />
+                                                <img src="/company_logos/snapppay.webp" alt="Country Comps Logo" className="size-8" />
+                                                <img src="/company_logos/tara.svg" alt="Country Comps Logo" className="size-8" />
+                                                <img src="/company_logos/wepod.jpg" alt="Country Comps Logo" className="size-8" />
+                                                <img src="/company_logos/blubank.webp" alt="Country Comps Logo" className="size-8" />
+                                                </>
+                                          )}    
+                                    </div>
+                                    <div className="flex w-full items-center justify-between rounded-md mt-4">
+                                          <Button component={Link} color="success" variant="contained" to="/" sx={{ textDecoration: 'none'  , padding : "4px 10px" , letterSpacing :"-0.5px" , fontWeight : "600"}}>درخواست وام</Button>
                                     </div>
                               </div>
                         </CardContent>
