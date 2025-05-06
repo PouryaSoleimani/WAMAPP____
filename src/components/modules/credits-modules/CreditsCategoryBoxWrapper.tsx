48602b7e6eecd778c2e94c63c623488b8ae3f4b5
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-06 02:07:30 -0400

    LOANS

--- src/components/modules/credits-modules/CreditsCategoryBoxWrapper.tsx
+++ src/components/modules/credits-modules/CreditsCategoryBoxWrapper.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import CreditsCategorySingleBox from './CreditsCategorySingleBox';
-import { BiWalletAlt } from 'react-icons/bi';
+import { BiHome, BiWalletAlt } from 'react-icons/bi';
+import { RiGlobalLine } from 'react-icons/ri';
 
 const CreditsCategoryBoxWrapper = () => {
   const [selectedCard, setSelectedCard] = React.useState(0);
 
   const cards = [
     { id: 1, title: 'وام تخصصی', desc: "هر کالایی که بخوای بخری با هر نیازی که داری ما بهت وام میدیم", isShowCountryComps: false, path: "/special-loans" ,icon :BiWalletAlt },
-    { id: 2, title: 'معرفی وام های کشوری', desc: "شرکت های معتبر وام دهی در کشور", isShowCountryComps: true, path: "/national-loans", icon :BiWalletAlt },
-    { id: 3, title: '  معرفی وام های استانی', desc: "وام دهندگان محلی در استان شما", isShowCountryComps: false, path: "/regional-loans", icon: BiWalletAlt },
+    { id: 2, title: 'معرفی وام های کشوری', desc: "شرکت های معتبر وام دهی در کشور", isShowCountryComps: true, path: "/national-loans", icon :RiGlobalLine },
+    { id: 3, title: '  معرفی وام های استانی', desc: "وام دهندگان محلی در استان شما", isShowCountryComps: false, path: "/regional-loans", icon: BiHome  },
   ];
   
   return (
--- src/components/modules/credits-modules/CreditsCategorySingleBox.tsx
+++ src/components/modules/credits-modules/CreditsCategorySingleBox.tsx
@@ -4,25 +4,26 @@ import { Link } from "react-router";
 
 import { IconType } from 'react-icons';
 
-interface CardProps { 
-  id: number; 
-  title: string; 
-  desc: string; 
-  path: string; 
-  icon: IconType
+interface CardProps {
+      id: number;
+      title: string;
+      desc: string;
+      path: string;
+      icon: IconType
 }
-interface CreditsCategorySingleBoxProps { 
-  card: CardProps; 
-  index: number; 
-  selectedCard: number; 
-  setSelectedCard: (index: number) => void; 
+interface CreditsCategorySingleBoxProps {
+      card: CardProps;
+      index: number;
+      selectedCard: number;
+      setSelectedCard: (index: number) => void;
 }
 
 
 
 const CreditsCategorySingleBox: React.FC<CreditsCategorySingleBoxProps> = ({ card, index, selectedCard, setSelectedCard }) => {
       return (
-            <Card key={card.id} sx={{ width: "100%", height: "24vh", margin: "10px 1.5px", borderRadius: "8px", boxShadow: 2, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "stretch", border: "none" }}>
+            <Card key={card.id} 
+                  sx={{ width: "100%", height: "24vh", margin: "10px 1.5px", borderRadius: "8px", boxShadow: 2, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "stretch", border: "none" }}>
                   <CardActionArea
                         onClick={() => setSelectedCard(index)}
                         data-active={selectedCard === index ? "" : undefined}
@@ -39,10 +40,9 @@ const CreditsCategorySingleBox: React.FC<CreditsCategorySingleBoxProps> = ({ car
                               <div className="flex flex-col items-start justify-start gap-2 basis-[90%]">
                                     <div className="flex items-center justify-between w-full">
                                     <h3 className="font-bold text-lg ">{card.title}</h3>
-                                    {React.createElement(card.icon, { className: "w-12 h-12 ml-2 translate-y-2 text-[var(--text-primary)]" })}
+                                          {React.createElement(card.icon, { className: "w-12 h-12 ml-2 translate-y-2 text-[var(--text-primary)]" })}
                                     </div>
                                     <p className="text-sm text-start w-[60%] text-[var(--text-secondary)]">{card.desc}</p>
-
                               </div>
 
                               <div className="flex w-full items-center justify-end basis-[10%]">
