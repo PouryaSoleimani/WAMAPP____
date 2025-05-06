3c6bb40143200c8b32f81440ec7453339411ea07
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-06 11:50:10 -0400

    LOANS

--- src/components/templates/FooterComponent.tsx
+++ src/components/templates/FooterComponent.tsx
@@ -24,7 +24,7 @@ const FooterItemsData = [
   return (
     <div className='text-[var(--text-primary)] bg-[var(--menu-bg)]  flex flex-col items-stretch' dir='rtl'>
 
-          <div dir="rtl" className="justify-between flex flex-col gap-y-4 gap-x-0 w-[95vw] mx-auto my-0 bg-[var(--menu-bg)]  ">
+          <div dir="rtl" className="justify-between flex flex-col gap-y-4 gap-x-0 w-[95vw] mx-auto my-0 bg-[var(--menu-bg)] mt-6 ">
               {FooterItemsData.map(item => (
                   <FooterAccordion key={item.id} title={item.title} desc={item.desc} icon={item.icon} />
               ))}
