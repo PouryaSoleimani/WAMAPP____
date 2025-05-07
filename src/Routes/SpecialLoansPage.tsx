28d1e367fc75bdd84a937df97e9baf82450cc7e5
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-06 20:47:11 -0400

    LOANS

--- src/Routes/SpecialLoansPage.tsx
+++ src/Routes/SpecialLoansPage.tsx
@@ -4,20 +4,42 @@ import FilterDropDownCities from '../components/modules/special-loans-modules/Fi
 import FilterDropDownSubjects from '../components/modules/special-loans-modules/FilterDropDownSubject'
 import FilterDropDownGuarantee from '../components/modules/special-loans-modules/FilterDropDownGuarantee'
 import BottomNavBarComponent from '../components/modules/bottom-navbar/BottomNavbarComponent'
+import SpecialLoanSingleBox from '../components/modules/special-loans-modules/SpecialLoanSingleBox'
+import { BiHome, BiWalletAlt } from 'react-icons/bi'
+import { RiGlobalLine } from 'react-icons/ri'
+import React from 'react'
 
+  const cards = [
+    { id: 1, title: 'وام تخصصی', desc: "هر کالایی که بخوای بخری با هر نیازی که داری ما بهت وام میدیم", isShowCountryComps: false, path: "/special-loans", icon: BiWalletAlt, tooltipdesc:"وام های ویژه وام اپ", tooltip:"وام های ویژه وام اپ" },
+    { id: 2, title: 'معرفی وام های کشوری', desc: "شرکت های معتبر وام دهی در کشور", isShowCountryComps: true, path: "/national-loans", icon :RiGlobalLine, tooltipdesc:"وام های کشوری", tooltip:"وام های کشوری" },
+    { id: 3, title: '  معرفی وام های استانی', desc: "وام دهندگان محلی در استان شما", isShowCountryComps: false, path: "/regional-loans", icon: BiHome, tooltipdesc:"وام های استانی", tooltip:"وام های استانی" },
+    { id: 4, title: '  معرفی وام های استانی', desc: "وام دهندگان محلی در استان شما", isShowCountryComps: false, path: "/regional-loans", icon: BiHome, tooltipdesc:"وام های استانی", tooltip:"وام های استانی" },
+    { id: 5, title: '  معرفی وام های استانی', desc: "وام دهندگان محلی در استان شما", isShowCountryComps: false, path: "/regional-loans", icon: BiHome, tooltipdesc:"وام های استانی", tooltip:"وام های استانی" },
+    { id: 6, title: '  معرفی وام های استانی', desc: "وام دهندگان محلی در استان شما", isShowCountryComps: false, path: "/regional-loans", icon: BiHome, tooltipdesc:"وام های استانی", tooltip:"وام های استانی" },
+    { id: 7, title: '  معرفی وام های استانی', desc: "وام دهندگان محلی در استان شما", isShowCountryComps: false, path: "/regional-loans", icon: BiHome, tooltipdesc:"وام های استانی", tooltip:"وام های استانی" },
+  ];
 const SpecialLoansPage = () => {
+
+    const [selectedCard, setSelectedCard] = React.useState(0)
+
+
   return (
-    <>
-      <NavBarComponent />
-      <section className="mx-auto relative inset-0 text-center z-10 h-auto mt-[54px] p-2 bg-[var(--inner-bg)]" dir="rtl">
-        <HeaderTitleComponent title="وام های تخصصی" />
-        <FilterDropDownCities/>
-        <FilterDropDownSubjects/>
-        <FilterDropDownGuarantee/>
-      </section>
-      <BottomNavBarComponent/>
-    </>
-  )
+		<>
+			<NavBarComponent />
+			<section className="mx-auto relative inset-0 text-center z-10 mt-[54px] p-2 bg-[var(--inner-bg)]" dir="rtl">
+				<HeaderTitleComponent title="وام های تخصصی" />
+				<FilterDropDownCities />
+				<FilterDropDownSubjects />
+				<FilterDropDownGuarantee />
+			</section>
+			<section className='p-3 mb-12 h-dvh'>
+				{cards.map((card, index) => (
+					<SpecialLoanSingleBox key={card.id} card={card} index={index} selectedCard={selectedCard} setSelectedCard={setSelectedCard} />
+				))}
+			</section>
+			<BottomNavBarComponent />
+		</>
+	)
 }
 
 export default SpecialLoansPage
