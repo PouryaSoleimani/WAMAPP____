7eff1891ae0578aa7a4a24f75a0adcb10936ce5d
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-09 01:03:20 -0400

    LOANS

--- src/components/modules/credits-modules/CreditsCategoryBoxWrapper.tsx
+++ src/components/modules/credits-modules/CreditsCategoryBoxWrapper.tsx
@@ -7,10 +7,37 @@ const CreditsCategoryBoxWrapper = () => {
   const [selectedCard, setSelectedCard] = React.useState(0);
 
   const cards = [
-    { id: 1, title: 'وام تخصصی', desc: "هر کالایی که بخوای بخری با هر نیازی که داری ما بهت وام میدیم", isShowCountryComps: false, path: "/special-loans", icon: BiWalletAlt, tooltipdesc:"وام های ویژه وام اپ", tooltip:"وام های ویژه وام اپ" },
-    { id: 2, title: 'معرفی وام های کشوری', desc: "شرکت های معتبر وام دهی در کشور", isShowCountryComps: true, path: "/national-loans", icon :RiGlobalLine, tooltipdesc:"وام های کشوری", tooltip:"وام های کشوری" },
-    { id: 3, title: '  معرفی وام های استانی', desc: "وام دهندگان محلی در استان شما", isShowCountryComps: false, path: "/regional-loans", icon: BiHome, tooltipdesc:"وام های استانی", tooltip:"وام های استانی" },
-  ];
+		{
+			id: 1,
+			title: 'وام تخصصی',
+			desc: 'هر کالایی که بخوای بخری با هر نیازی که داری ما بهت وام میدیم',
+			isShowCountryComps: false,
+			path: '/special-loans',
+			icon: BiWalletAlt,
+			tooltipdesc: 'وام های ویژه وام اپ',
+			tooltip: 'وام های ویژه وام اپ'
+		},
+		{
+			id: 2,
+			title: 'معرفی وام های کشوری',
+			desc: 'شرکت های معتبر وام دهی در کشور',
+			isShowCountryComps: true,
+			path: '/national-loans',
+			icon: RiGlobalLine,
+			tooltipdesc: 'وام های کشوری',
+			tooltip: 'وام های کشوری'
+		},
+		{
+			id: 3,
+			title: '  معرفی وام های استانی',
+			desc: 'وام دهندگان محلی در استان شما',
+			isShowCountryComps: false,
+			path: '/regional-loans',
+			icon: BiHome,
+			tooltipdesc: 'وام های استانی',
+			tooltip: 'وام های استانی'
+		}
+	]
 
   return (
 		<div className="flex flex-col h-[75dvh] place-content-around p-0.5">
