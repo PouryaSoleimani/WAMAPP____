7c8a4ba5f813e8aa34ea88b653bf93e389f89af2
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-12 12:59:02 +0330

    SPECIAL LOANS

--- src/Routes/LoanAccessPage.tsx
+++ src/Routes/LoanAccessPage.tsx
@@ -25,17 +25,16 @@ const LoanAccessPage = ( ) => {
                     <h3 id="HEADER__TITLE" className="p-3 rounded text-lg w-full flex items-center justify-between border"> مبلغ <p>40,000,000 ریال</p> </h3>
                     <ul id="INNER" className='bg-[var(--inner-bg)]  w-full text-sm font-extralight  rounded-lg list-disc list-inside p-3 space-y-4 text-[var(--text-secondary)]'>
                         <li className="w-full flex items-center justify-between border-b pb-4">مبلغ تسهیلات <p>40,000,000 ریال</p></li>
-                        <li className="w-full flex items-center justify-between border-b pb-4">میزان سود <p>40,000,000 ریال</p></li>
-                        <li className="w-full flex items-center justify-between border-b pb-4">مبلغ تسهیلات <p>40,000,000 ریال</p></li>
-                        <li className="w-full flex items-center justify-between border-b pb-4">مبلغ تسهیلات <p>40,000,000 ریال</p></li>
-                        <li className="w-full flex items-center justify-between border-b pb-4">مبلغ تسهیلات <p>40,000,000 ریال</p></li>
+                        <li className="w-full flex items-center justify-between border-b pb-4">میزان سود <p>14 درصد</p></li>
+                        <li className="w-full flex items-center justify-between border-b pb-4">مبلغ ماهانه <p>1,225,000 ریال</p></li>
+                        <li className="w-full flex items-center justify-between border-b pb-4">سود <p>1,225,000 ریال</p></li>
+                        <li className="w-full flex items-center justify-between border-b pb-4 text-center">در صورت تغییر تصمیم در تعداد باز‍برداخت اقساط ُ‌تصمیم شما قابل اجرا می باشد .</li>
                     </ul>
                 </div>
-                <Button
-                    variant='contained'
-                    color='success'
-                    sx={{ width: "100%", margin: "2px auto", fontWeight: "bold", padding: "13px", ":hover": { backgroundColor: "darkgreen" } }}
-                >
+                <Button variant='contained' color='primary' sx={{ width: "100%", margin: "2px auto", fontWeight: "bold", padding: "13px", ":hover": { backgroundColor: "darkgreen" } }} >
+                    اعتبار سنجی
+                </Button>
+                <Button variant='contained' color='success' sx={{ width: "100%", margin: "2px auto", fontWeight: "bold", padding: "13px", ":hover": { backgroundColor: "darkgreen" } }} >
                     دریافت تسهیلات
                 </Button>
             </div>
