40f63e61e6a41520ea21e639099e37b98589a174
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-14 07:06:10 +0330

    SPECIAL LOANS

--- src/components/modules/credits-modules/CreditsMenuList.tsx
+++ src/components/modules/credits-modules/CreditsMenuList.tsx
@@ -20,26 +20,18 @@ const StyledMenu = styled((props: MenuProps) => (
     />
 ))(({ theme }) => ({
     '& .MuiPaper-root': {
-        borderRadius: 6,
+
+        borderRadius: 20,
         marginTop: theme.spacing(1),
         minWidth: "90%",
         color: 'rgb(55, 65, 81)',
-        boxShadow:
-            'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-        '& .MuiMenu-list': {
-            padding: '0 0',
-        },
+        padding: "10px 4px",
+        boxShadow: 'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+        '& .MuiMenu-list': { padding: '0 0', },
         '& .MuiMenuItem-root': {
-            '& .MuiSvgIcon-root': {
-                fontSize: 18,
-                color: theme.palette.text.secondary,
-                marginRight: theme.spacing(1.5),
-            },
+            '& .MuiSvgIcon-root': { fontSize: 18, color: theme.palette.text.secondary, marginRight: theme.spacing(1.5), },
             '&:active': {
-                backgroundColor: alpha(
-                    theme.palette.primary.main,
-                    theme.palette.action.selectedOpacity,
-                ),
+                backgroundColor: alpha(theme.palette.primary.main, theme.palette.action.selectedOpacity,),
             },
         },
         ...theme.applyStyles('dark', {
@@ -73,16 +65,15 @@ export function CreditsMenuList() {
                 open={open}
                 onClose={handleClose}
             >
-                <MenuItem onClick={handleClose} disableRipple sx={{ display: "flex", alignItems: "center ", justifyContent: "space-between", bgcolor: "lightgray", margin: "5px", borderRadius: "30px" }}>
-                    <span className='flex items-center justify-start gap-2'>
-                        <CiCircleCheck />
-                        بازبرداخت ۱ ماه
-                    </span>
-                    <span>۸۵۶,۰۰۰ تومان</span>
-                </MenuItem>
+                {Array.from({ length: 12 }).map((_, index) => (
+                    <MenuItem key={crypto.randomUUID()} onClick={handleClose} disableRipple sx={{ display: "flex", alignItems: "center ", justifyContent: "space-between", bgcolor: "lightgray", margin: "5px", padding: "4px 20px", borderRadius: "30px" }}>
+                        <span className='flex items-center justify-start gap-2 tracking-tight'> <CiCircleCheck /> بازبرداخت {(index + 1).toLocaleString()}  ماه </span>
+                        <span className='VazirBold'>۸۵۶,۰۰۰ تومان</span>
+                    </MenuItem>
+                ))}
 
             </StyledMenu>
-        </div>
+        </div >
     );
 }
 
